94b7772aa7f6ba58b55dbb50fb66904bbc633658
npazderov <nikola_pazderov@abv.bg>
2016-12-30 19:50:00 +0200

    Add files via upload

---
+++ ZAD1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int func(char *string)
+{
+    int counter=0;
+    int i;
+    for(int i=0; string[i]!=0 ; i++)
+    {
+	if(string[i]!=' ')
+	{
+        counter++;
+	}
+        if(counter == '/n')
+        {
+            break;
+        }
+
+    }
+
+    cout<<"broi simvoli: " << counter <<endl;
+    cout<<"1vi simvol : " << string[0]<<endl;
+    cout<<"Sreden simvol : " << string[counter/2]<<endl;
+    cout<<"Posleden simvol: " <<string[counter-1] <<endl;
+
+    return counter;
+}
+int main()
+{
+    char string[50];
+    cout<<" Enter Text: ";
+    cin.getline(string,50);
+    cout<<func(string)<<endl;
+
+
+
+}
